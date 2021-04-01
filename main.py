c6265d833640c4b041ef711c2110da8c21f2308f
sqEquat <nikitatresh@gmail.com>
2021-04-01 19:50:26 +0300

    Add get rate function

--- main.py
+++ main.py
@@ -42,6 +42,16 @@ class SalaryInterface(MyConnector):
         query = "INSERT INTO schedule (employeeId, date, hours) VALUES (%(employeeId)s, %(date)s, %(hours)s)"
         self.insert_data(query, data)
 
+    def get_position_rate(self, employee_id):
+        query = f"""SELECT rate FROM salary_calc.position 
+                    WHERE id = (SELECT positionId FROM employee WHERE id = {employee_id})"""
+        self.query_exe(query)
+
+        return [x for x in self.cursor][0][0]
+
+    def calc_salary(self, employee_id, month, nominal_hours):
+        pass
+
 
 def main():
 
@@ -54,10 +64,12 @@ def main():
         )
 
         # salary.add_employee({'name': 'Stanislav G', 'positionId': 2})
-        today = date(2020, 3, 27)
-        salary.add_employee_schedule({'employeeId': '1', 'date': today, 'hours': 8})
-        salary.show_table('employee')
-        salary.show_table('schedule')
+        # today = date(2020, 3, 27)
+        # salary.add_employee_schedule({'employeeId': '1', 'date': today, 'hours': 8})
+        # salary.show_table('employee')
+        # salary.show_table('schedule')
+        rate = salary.get_position_rate(1)
+        print(rate)
 
     except Error as e:
         print(e)
