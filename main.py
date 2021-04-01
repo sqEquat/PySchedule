79b022810c3e13565757e27328b6fe3d0bd238ff
sqEquat <nikitatresh@gmail.com>
2021-04-01 19:23:41 +0300

    Add some functions to SalaryInterface

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import atexit
 from getpass import getpass
+from datetime import date
+
 from mysql.connector import connect, Error
 
 
@@ -16,21 +18,46 @@ class MyConnector:
         self.cursor.execute(query)
         self.connection.commit()
 
+    def insert_data(self, query, data):
+        self.cursor.execute(query, data)
+        self.connection.commit()
+
+
+class SalaryInterface(MyConnector):
+    """ Interface for simple access sql functionality """
+    def __init__(self, param):
+        super().__init__(param)
+
+    def show_table(self, table):
+        query = f"SELECT * FROM {table}"
+        self.query_exe(query)
+        for row in self.cursor:
+            print(row)
+
+    def add_employee(self, data):
+        query = "INSERT INTO employee (name, positionId) VALUES (%(name)s, %(positionId)s)"
+        self.insert_data(query, data)
+
+    def add_employee_schedule(self, data):
+        query = "INSERT INTO schedule (employeeId, date, hours) VALUES (%(employeeId)s, %(date)s, %(hours)s)"
+        self.insert_data(query, data)
+
 
 def main():
 
     try:
-        connection = MyConnector({
+        salary = SalaryInterface({
                 'host': 'localhost',
                 'user': 'root',
                 'password': getpass(),
                 'database': 'salary_calc'}
         )
 
-        query = "SELECT * FROM salary_calc.employee"
-        connection.query_exe(query)
-        for row in connection.cursor:
-            print(row)
+        # salary.add_employee({'name': 'Stanislav G', 'positionId': 2})
+        today = date(2020, 3, 27)
+        salary.add_employee_schedule({'employeeId': '1', 'date': today, 'hours': 8})
+        salary.show_table('employee')
+        salary.show_table('schedule')
 
     except Error as e:
         print(e)
