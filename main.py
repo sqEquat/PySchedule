86d08b9ae6c32c0db9fac67f2ee9516dba2d7cb8
sqEquat <nikitatresh@gmail.com>
2021-04-08 00:45:23 +0300

    Complete salary calculation function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from calendar import Calendar
 import os
 
-from getpass import getpass
+from datetime import date
+from calendar import Calendar
 from mysql.connector import Error
 
 from myconnector import MyConnector
@@ -9,11 +9,13 @@ from myconnector import MyConnector
 
 class SalaryInterface(MyConnector):
     """ Interface for simple access sql functionality """
+
     def __init__(self, param):
         super().__init__(param)
 
     def show_table(self, table):
         """ Display all rows of the table """
+
         query = f"SELECT * FROM {table}"
         self.select_query(query)
         for row in self.cursor:
@@ -54,8 +56,8 @@ class SalaryInterface(MyConnector):
     def get_total_month_worked_out_hours(self, employee_id, year, month):
         """ Return sum of hours from schedule table  of employee in the month of the year """
 
-        query = f"""select sum(sch.hours) from schedule sch
-                    where month(sch.date) = {month} and year(sch.date) = {year} and sch.employeeId = {employee_id}"""
+        query = f"""SELECT SUM(sch.hours) FROM schedule sch
+                    WHERE MONTH(sch.date) = {month} AND YEAR(sch.date) = {year} AND sch.employeeId = {employee_id}"""
         self.select_query(query)
 
         # fetchone() return a tuple with one value, so [0] gets the value
@@ -65,9 +67,16 @@ class SalaryInterface(MyConnector):
         """ Insert row with information of employee payment in tne month of the year  """
 
         employee_id = data['employee_id']
-        rate = self.get_position_rate(data['employee_id'])
-        pass
+        payment_date = date.today()
+        total_hours = self.get_total_month_worked_out_hours(employee_id, data['year'], data['month'])
+        nominal_hours = data['nominal_hours']
+        rate = self.get_position_rate(employee_id)
+        payment_sum = (total_hours / nominal_hours) * rate
+        status_id = 2
 
+        query = f"""INSERT INTO payment (employeeId, date, totalHours, nominalHours, positionRate, sum, statusId)
+                    VALUES ({employee_id}, '{payment_date}', {total_hours}, {nominal_hours}, {rate}, {payment_sum:.2f}, {status_id})"""
+        self.insert_query(query)
 
 
 def main():
@@ -81,10 +90,11 @@ def main():
         )
 
         # salary.add_employee({'name': 'Anton C', 'position_id': 1})
-        salary.fill_schedule(12, 2021, 4, 5)
+        # salary.fill_schedule(12, 2021, 4, 5)
         # salary.show_table('schedule')
 
-        print(salary.get_total_month_worked_out_hours(12, 2021, 4))
+        salary.calc_salary({'employee_id': 1, 'month': 4, 'year': 2021, 'nominal_hours': 160})
+        salary.show_table('payment')
 
     except Error as e:
         print(e)
