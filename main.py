a6ea17a7491709d0793ea1c331290309fcc75430
sqEquat <nikitatresh@gmail.com>
2021-04-07 19:24:49 +0300

    Separate MyConnector in module

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import atexit
 from getpass import getpass
 from datetime import date
+from mysql.connector import Error
 
-from mysql.connector import connect, Error
-
-
-class MyConnector:
-    """ Wrapper for connector from mysql.connector """
-    def __init__(self, param):
-        self.connection = connect(**param)
-        self.cursor = self.connection.cursor(buffered=True)
-
-        atexit.register(self.cursor.close)
-        atexit.register(self.connection.close)
-
-    def query_exe(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
-
-    def insert_data(self, query, data):
-        self.cursor.execute(query, data)
-        self.connection.commit()
+from myconnector import MyConnector
 
 
 class SalaryInterface(MyConnector):
