8d7f7896561dba24503eea7ce8f2b9c037781961
sqEquat <nikitatresh@gmail.com>
2021-04-09 10:39:53 +0300

    Change default tab on app startup

--- design.py
+++ design.py
@@ -93,7 +93,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.tabBar.setCurrentIndex(3)
+        self.tabBar.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
